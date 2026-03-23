53f898649b88e484c0d9a08e21f973fd4d7b3d04
EmilSG <emilsvardgronvikacc@gmail.com>
2026-03-23 13:46:13 +0100

    Rewrite 1.19.3-4

--- forge/src/main/java/net/emilsg/forge/BWForge.java
+++ forge/src/main/java/net/emilsg/forge/BWForge.java
@@ -1,5 +1,6 @@
 package net.emilsg.forge;
 
+
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 
@@ -9,11 +10,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(BackportedWolvesCommon.MOD_ID)
 public final class BWForge {
     public BWForge() {
-        EventBuses.registerModEventBus(
-                BackportedWolvesCommon.MOD_ID,
-                FMLJavaModLoadingContext.get().getModEventBus()
-        );
-
+        EventBuses.registerModEventBus(BackportedWolvesCommon.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         BackportedWolvesCommon.init();
     }
 }
